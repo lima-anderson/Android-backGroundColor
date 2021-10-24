04bf489cec67a5d8f610b3d7f1aded0dbe44a420
lima-anderson <msgli34@gmail.com>
2021-10-23 22:59:57 -0300

    first commit

---
+++ app/src/main/java/com/example/cores/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.cores
+
+import android.graphics.Color
+import android.graphics.ColorMatrix
+import android.graphics.drawable.ColorDrawable
+import android.graphics.drawable.Drawable
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var tvNomeDaCor: TextView
+    private lateinit var layout: ConstraintLayout
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        this.tvNomeDaCor = findViewById(R.id.tvNomeDaCor)
+        this.layout = findViewById(R.id.layout)
+
+
+        this.layout.setOnClickListener{
+            layout.setBackgroundColor(Color.rgb(Cores().r(), Cores().g(), Cores().b()))
+            this.tvNomeDaCor.text = "rgb(" + Cores().r().toString() + ", " + Cores().g().toString() + ", " +  Cores().b().toString() + ")"
+        }
+    }
+
+    inner class Cores {
+        fun r(): Int {
+            return Random.nextInt(1,256);
+        }
+        fun g(): Int {
+            return Random.nextInt(1,256);
+        }
+        fun b (): Int {
+            return Random.nextInt(1,256);
+        }
+    }
+}
